5299f72e9f2fd471a79d521df3c5411c7774f770
Alejandro G. Lacasa <alejandro.lacasa@nexum.com>
2023-12-22 11:26:39 +0100

    Add client-side rendering for LotteryChecker component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+'use client'
+
 import { LotteryChecker } from '@/components/LotteryChecker'
 import { Snowflakes } from '@/components/Snowflakes'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [isSSR, setIsSSR] = useState<boolean>(true)
+
+  useEffect(() => {
+    setIsSSR(false)
+  }, [])
+
   return (
     <main suppressHydrationWarning>
       <Snowflakes />
-      <LotteryChecker />
+      {!isSSR && <LotteryChecker />}
     </main>
   )
 }
