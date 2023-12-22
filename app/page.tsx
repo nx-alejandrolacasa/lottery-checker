eab034a955a003d66a5ad2d2534b923b24e013f7
Alejandro G. Lacasa <alejandro.lacasa@nexum.com>
2023-12-22 11:25:50 +0100

    Refactor page.tsx to remove unused code and update component rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,11 @@
-'use client'
-
 import { LotteryChecker } from '@/components/LotteryChecker'
 import { Snowflakes } from '@/components/Snowflakes'
-import { useEffect, useState } from 'react'
 
 export default function Home() {
-  const [isSSR, setIsSSR] = useState<boolean>(true)
-
-  useEffect(() => {
-    setIsSSR(false)
-  }, [])
-
   return (
     <main suppressHydrationWarning>
       <Snowflakes />
-      {!isSSR && <LotteryChecker />}
+      <LotteryChecker />
     </main>
   )
 }
